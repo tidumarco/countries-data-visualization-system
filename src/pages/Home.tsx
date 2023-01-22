e794a1eb41aa214fb49ade3d1397bf59245925dc
Marco Tidu <tidumarco@gmail.com>
2023-01-22 13:57:53 +0200

    Starting styling in homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,41 @@
+import { Paper, Typography } from "@mui/material";
 import React, { useEffect } from "react";
-
+import Image from "../images/world.jpeg";
 import { Link } from "react-router-dom";
+
+import "../App.css";
 
 export default function Home(props: any) {
   useEffect(() => (document.title = props.title), [props.title]);
+
+  const linkStyle = {
+    textDecoration: "none",
+  };
+
+  const paperStyle = {
+    backgroundImage: `url(${Image})`,
+    backgroundSize: "cover",
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+  };
   return (
-    <>
-      <h1>This is the home page</h1>
-      <ul>
-        <li>
-          <Link to="data-by-region">Data by region</Link>
-        </li>
-        <li>
-          <Link to="data-by-country">Data by country</Link>
-        </li>
-      </ul>
-    </>
+    <Paper className="paper-background" style={paperStyle}>
+      <Typography variant="h3">Welcome to our system</Typography>
+      <Typography variant="body2">
+        <ul>
+          <li>
+            <Link style={linkStyle} to="data-by-region">
+              Data by region
+            </Link>
+          </li>
+          <li>
+            <Link style={linkStyle} to="data-by-country">
+              Data by country
+            </Link>
+          </li>
+        </ul>
+      </Typography>
+    </Paper>
   );
 }
