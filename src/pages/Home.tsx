d84d03cf72dde1c74165822e987854cfbe5caf38
Marco Tidu <tidumarco@gmail.com>
2023-02-20 09:06:15 +0200

    Fixes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
-import BackButton from "../components/BackButton";
 
 export default function Home() {
   return (
